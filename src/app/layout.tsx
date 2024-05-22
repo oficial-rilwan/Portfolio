3e1a00aeacdc8c2f59474b3a7cd90ca752d5e6b0
Rilwan Aribidesi <aribidesirilwan1477@gmail.com>
2024-05-22 10:50:32 +0100

    added seo keywords

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,33 @@ const inter = Poppins({
 });
 
 export const metadata: Metadata = {
-  title: "Rilwan Aribidesi",
-  description: "Personal Portfolio",
+  title: "Rilwan Aribidesi - Portfolio",
+  description: `Solution-driven Full-stack Engineer with over 3 years of working experience building customer-focused products
+and services. Designed and developed multiple web and mobile apps across multiple APIs, third-party integrations
+and databases. Thrive in working in a fast-paced, high-tech environment with cross-functional teams using agile
+methodology.`,
+  keywords: [
+    "frontend developer",
+    "frontend engineer",
+    "full-stack engineer",
+    "full-stack developer",
+    "typescript",
+    "nodejs",
+    "reactjs",
+    "nextjs",
+    "dev",
+    "daily.dev",
+  ],
+  icons:
+    "https://rilwanthedev.netlify.app/_next/image?url=%2Favatar-1.jpg&w=1920&q=75",
+
+  authors: [
+    {
+      name: "Rilwan Aribidesi",
+      url: "https://www.linkedin.com/in/rilwanthedev/",
+    },
+  ],
+  category: "Personal Portfolio",
 };
 
 export default function RootLayout({
