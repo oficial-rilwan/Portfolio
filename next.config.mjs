5535ffd697a6a3b47d5d355c98da74bb27f335fc
oficial-rilwan <aribidesirilwan1477@gmail.com>
2025-01-07 18:05:42 +0100

    build command updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   images: {
     domains: ["gettiket.com", "jureb.com", "fresible.com", "fresible.link"],
     remotePatterns: [
