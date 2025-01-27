228d61f555686aa9cefc2157735c53d38472009b
oficial-rilwan <aribidesirilwan1477@gmail.com>
2025-01-27 09:42:42 +0100

    added reactjs image

--- src/static/technologies.ts
+++ src/static/technologies.ts
@@ -1,6 +1,7 @@
 const TECHNOLOGIES = [
   { name: "TypeScript", image: "/ts.svg", bg: "#f1f1f1" },
   { name: "JavaScript", image: "/js.svg", bg: "#f1f1f1" },
+  { name: "React.js", image: "/react-logo.png", bg: "#f1f1f1" },
   { name: "Next.js", image: "/next.svg", bg: "#f1f1f1" },
   { name: "Node.js", image: "/node.svg", bg: "#f1f1f1" },
   { name: "Express.js", image: "/express.svg", bg: "#f1f1f1" },
